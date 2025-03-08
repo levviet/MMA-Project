eee8c75ebbeb271af98f50f78247c41c6a10bf14
vietlvhe180475 <vietlvhe180475@fpt.edu.vn>
2025-03-09 01:18:55 +0700

    Tạo UI cho HomePage

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,20 +1,58 @@
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-        <Text className="font-bold my-10 font-rubik text-3xl">Welcome to Vịt</Text>
-        <Link href="/sign-in">Sign In</Link>
-        <Link href="/explore">Explore</Link>
-        <Link href="/profile">Profile</Link>
-        <Link href="/properties/1">Property</Link>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+              <Text className="text-base font-rubik-medium text-black-300">Vịt Hehe</Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+
+        <Search />
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
+        </View>
+
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">Our Recommendation</Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Filters/>
+
+        <View className="flex flex-row gap-5 mt-5">
+          <Card/>
+          <Card/>
+        </View>
+
+      </View>
+    </SafeAreaView>
   );
 }
--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -41,7 +41,7 @@ const Profile = () => {
           onPress: async () => {
             try {
               const result = await logout();
-  
+
               if (result) {
                 Alert.alert("Success", "You have been logged out successfully");
                 refetch();
@@ -56,50 +56,56 @@ const Profile = () => {
       ]
     );
   };
-  
+
 
   return (
-    <SafeAreaView className='h-full bg-white'>
+    <SafeAreaView className="h-full bg-white">
       <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerClassName='pb-32 px-7'
+        contentContainerClassName="pb-32 px-7"
       >
-        <View className='flex flex-row items-center justify-between mt-5'>
-          <Text className='text-xl font-rubik-bold'>
-            Profile
-          </Text>
-          <Image source={icons.bell} className='size-5' />
+        <View className="flex flex-row items-center justify-between mt-5">
+          <Text className="text-xl font-rubik-bold">Profile</Text>
+          <Image source={icons.bell} className="size-5" />
         </View>
 
-        <View className='flex-row justify-center flex mt-5'>
-          <View className='flex flex-col items-center relative mt-5'>
-            <Image source={{ uri: user?.avatar }} className='size-44 relative rounded-full' />
-
-            <TouchableOpacity className='absolute bottom-11 right-2'>
-              <Image source={icons.edit} className='size-9' />
+        <View className="flex flex-row justify-center mt-5">
+          <View className="flex flex-col items-center relative mt-5">
+            <Image
+              source={{ uri: user?.avatar }}
+              className="size-44 relative rounded-full"
+            />
+            <TouchableOpacity className="absolute bottom-11 right-2">
+              <Image source={icons.edit} className="size-9" />
             </TouchableOpacity>
 
-            <Text className='text-2xl font-rubik-bold mt-2'> {user?.name} </Text>
-          </View>
-
-          <View className='flex flex-col mt-10'>
-            <SettingsItem icon={icons.calendar} title='My Bookings' />
-            <SettingsItem icon={icons.wallet} title='Payments' />
+            <Text className="text-2xl font-rubik-bold mt-2">{user?.name}</Text>
           </View>
         </View>
 
-        <View className='flex flex-col mt-5 border-t pt-5 border-primary-200'>
+        <View className="flex flex-col mt-10">
+          <SettingsItem icon={icons.calendar} title="My Bookings" />
+          <SettingsItem icon={icons.wallet} title="Payments" />
+        </View>
+
+        <View className="flex flex-col mt-5 border-t pt-5 border-primary-200">
           {settings.slice(2).map((item, index) => (
             <SettingsItem key={index} {...item} />
           ))}
         </View>
 
-        <View className='flex flex-col mt-5 border-t pt-5 border-primary-200'>
-          <SettingsItem icon={icons.logout} title="Logout" textStyle={"text-danger"} showArrow={false} onPress={handleLogout} />
+        <View className="flex flex-col border-t mt-5 pt-5 border-primary-200">
+          <SettingsItem
+            icon={icons.logout}
+            title="Logout"
+            textStyle="text-danger"
+            showArrow={false}
+            onPress={handleLogout}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default Profile
